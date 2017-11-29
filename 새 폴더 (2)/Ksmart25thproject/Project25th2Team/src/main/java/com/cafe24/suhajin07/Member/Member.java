661ba21108d4303ec689694c350a2c997d2eb8da
smart-202-1 <smart-202-1@smart-202-1-PC>
2017-11-29 10:46:19 +0900

    20171129 Test

---
+++ 새 폴더 (2)/Ksmart25thproject/Project25th2Team/src/main/java/com/cafe24/suhajin07/Member/Member.java
@@ -0,0 +1,85 @@
+package com.cafe24.suhajin07.Member;
+
+public class Member {
+
+	private int memberCode;
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String memberPhone;
+	private String memberEmail;
+	private String memberPostNum;
+	private String memberAddress;
+	private String memberDetailAddress;
+	private String memberGrade;
+	public int getMemberCode() {
+		return memberCode;
+	}
+	public void setMemberCode(int memberCode) {
+		this.memberCode = memberCode;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberPhone() {
+		return memberPhone;
+	}
+	public void setMemberPhone(String memberPhone) {
+		this.memberPhone = memberPhone;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberPostNum() {
+		return memberPostNum;
+	}
+	public void setMemberPostNum(String memberPostNum) {
+		this.memberPostNum = memberPostNum;
+	}
+	public String getMemberAddress() {
+		return memberAddress;
+	}
+	public void setMemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+	public String getMemberDetailAddress() {
+		return memberDetailAddress;
+	}
+	public void setMemberDetailAddress(String memberDetailAddress) {
+		this.memberDetailAddress = memberDetailAddress;
+	}
+	public String getMemberGrade() {
+		return memberGrade;
+	}
+	public void setMemberGrade(String memberGrade) {
+		this.memberGrade = memberGrade;
+	}
+	@Override
+	public String toString() {
+		return "Member [memberCode=" + memberCode + ", memberId=" + memberId + ", memberPw=" + memberPw
+				+ ", memberName=" + memberName + ", memberPhone=" + memberPhone + ", memberEmail=" + memberEmail
+				+ ", memberPostNum=" + memberPostNum + ", memberAddress=" + memberAddress + ", memberDetailAddress="
+				+ memberDetailAddress + ", memberGrade=" + memberGrade + "]";
+	}
+	
+	
+	
+}
