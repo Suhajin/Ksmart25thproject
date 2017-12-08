9e825db9ce4e006c13a3f4d4009eb28cb897b291
Administrator <Administrator@MSDN-SPECIAL>
2017-12-08 11:37:05 +0900

    DetailGoal insert&list MidGoal 오류수정

--- Project25th2Team/src/main/java/com/cafe24/suhajin07/DetailGoal/DetailGoalController.java
+++ Project25th2Team/src/main/java/com/cafe24/suhajin07/DetailGoal/DetailGoalController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +15,17 @@ import com.cafe24.suhajin07.Member.Member;
 
 @Controller
 public class DetailGoalController {
+	@Autowired
 	DetailGoalService dgService;
 	
 	//세부목표 리스트
 	@RequestMapping(value="/DetailGoalList", method=RequestMethod.GET)
-	public String FinalGoal(Model model, @RequestParam("memberId") String memberId) {
+	public String DetailGoal(Model md, @RequestParam("memberId") String memberId) {
 		System.out.println("세부목표 in 목표설정페이지");
 		System.out.println(memberId);
-		List<DetailGoal> list= dgService.listDetailGoal(memberId);
-		model.addAttribute("listDetailGoal", list);
+		List<DetailGoal> list = dgService.listDetailGoal(memberId);
+		md.addAttribute("listDetailGoal", list);
+		System.out.println(list);
 		return "Detail_Goal/DetailGoal";
 		
 	}
@@ -35,10 +38,10 @@ public class DetailGoalController {
 		}
 	//세부목표 등록
 	@RequestMapping(value="/AddDetailGoal", method=RequestMethod.POST)
-	public String addDetailGoal(HttpSession session, DetailGoal dg) {
+	public String addDetailGoal(HttpSession ss, DetailGoal dg) {
 		System.out.println("DetailGoalController 성공"+dg);
 		dgService.addDetailGoal(dg);
-		return "redirect:/DetailGoalList?memberId="+((Member)session.getAttribute("Member")).getMemberId();		
+		return "redirect:/DetailGoalList?memberId="+((Member)ss.getAttribute("Member")).getMemberId();		
 		}
 }
 
